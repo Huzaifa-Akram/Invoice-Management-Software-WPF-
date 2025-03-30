e8f50d2ce193bc1150b1fde5f2997c60ec82354c
Huzaifa-Akram <huzaifaakram121@gmail.com>
2025-03-30 15:24:29 +0500

    Add project files.

---
+++ Software/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using Software.Data;
+
+namespace Software
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //DatabaseHelper dbHelper = new DatabaseHelper();
+
+        }
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //string storedLicenseKey = null;
+            string storedLicenseKey = (string)Registry.GetValue(
+                @"HKEY_CURRENT_USER\Software\SoftwareName", "StoredLicenseKey", null);
+
+            if (!string.IsNullOrEmpty(storedLicenseKey))
+            {
+                // Open MainWindow directly if a valid key exists
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.MainWindow = mainWindow;
+            }
+            else
+            {
+                // Open LoginWindow if no valid key exists
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+                this.MainWindow = loginWindow;
+            }
+        }
+
+    }
+}
